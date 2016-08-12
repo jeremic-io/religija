5683929d2d16ae09ba70b23ead47f71d7b4b3681
Dragan Jeremic <jeremic.dragan1991@gmail.com>
2016-08-12 13:22:54 +0200

    Prepare for deploy on Heroku.

--- index.php
+++ index.php
@@ -83,6 +83,7 @@
         <p>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
+        <a href="#" class="btn btn-intro">Pročitaj Vise</a>
       </div>
     </div>
 
@@ -90,27 +91,7 @@
       <div class="container">
         <div class="row">
           <div class="col-lg-8">
-            <p>
-              Dobrodošli na sajt religija.net. Mi smo nezavisna grupa ljudi koje zanimaju odgovori na neka od važnih životnih pitanja. Obični smo ljudi iz komšiluka koji su se upoznali preko interneta. Shvatili smo da imamo dosta zajedničkih tema, postali smo prijatelji i odlučili da zajedno služimo Bogu. To što smo nezavisni nam omogućava da ne padnemo pod uticaj nekog vođe ili organizacije. Naš jedini vođa je Bog. Ne pripadamo ni jednoj denominaciji, a sve zemaljske organizacije doživljavamo uslovno. Reč crkva (na hebrejskom sinagoga, a na grčkom eklesia) označava skup naroda, mi verujemo da svi koji prihvate Hrista kao svog spasitelja i koji oslanjaju svoju veru isključivo na Svetom Pismu(link) se smatraju Božijom Crkvom.
-            </p>
-            <p>
-              Misija ovog sajta je da se Istina približi ljudima i da na odgovarajući način slavimo našeg Gospoda i Spasitelja, Isusa Hrista. On je "put, istina i život" (Jovan 14:6), i Njegova reč je Istina (Jovan 17:17) i život (Jovan 6:63 ). Nije nam namera da nikoga osuđujemo, vređamo ili napadamo već samo da govorimo istinu i ništa drugo osim istine, a Bog neka bude taj koji će svima da nam sudi. Nismo savršeni i ne mislimo da sve znamo. Samo je Bog taj koji sve zna. Uvek spremno prihvatamo kritike na naš račun i ostavljamo ih javno da ih svi vide pod uslovom da su bez ružnih reči. Materijal koji ćete naći na ovom sajtu smo skupljali sa različitih izvora. Zalažemo se za širenje Dobre Vesti mirnim putem, bez nasilja i vulgarnosti.
-            </p>
-            <p>
-              Zapisano je da treba da istražujemo Pismo kako bi pronašli pravi put, i da sve izjave ispitujemo pomoću Svetog Pisma. Treba da se istrajemo u Božijoj Reči i da se pazimo lažnih doktrina na koje nas Bog upozorava.
-            </p>
-            <p>
-              Bog želi da Mu služimo "duhom i istinom" (Jovan 4:23-24) i da živimo po Njegovom primeru (1. Jovanova 2:6-7, 1. Korinćanima 11:1-2). On ne želi da mi, a posebno naša deca, nemamo pogrešnu predstavu o Njemu. Bog ne želi da religija bude skup običaja, obreda i gozbi! Držimo se onoga što je zapisano u Bibliji i slavimo živog Boga u svojim srcima svakoga dana. Kada je Isus došao na ovu Zemlju, uputio je pitanje svima onima koji su tradiciju smatrali važnijom od Božjih zapovesti: „Zašto vi prestupate zapovest Božju za običaje svoje?“ (Matej 15:3).
-            </p>
-            <p>
-              Dela 5:29 “A Petar i drugi apostoli na to su rekli: „Bogu se moramo pokoravati, a ne ljudima.”
-            </p>
-            <p>
-              Marko 7:7-9 „No zaludu me poštuju učeći naukama i zapovestima ljudskim. Jer ostaviste zapovesti Božje, a držite običaje ljudske,... dobro ukidate zapovest Božju da svoj običaj sačuvate."
-            </p>
-            <p>
-              Dela 20:29-30 "Ja znam da će posle mog odlaska među vas ući okrutni vukovi koji neće imati obzira prema stadu. I između vas samih dići će se ljudi koji će iznositi iskrivljena učenja da bi odvukli učenike za sobom."
-            </p>
+
           </div>
         </div>
       </div>
