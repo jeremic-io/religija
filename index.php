4145a6dc6eeba801c5aad4edcc9f67bfa9ff568f
Dragan Jeremic <jeremic.dragan1991@gmail.com>
2016-08-16 09:36:35 +0200

    Add Intro Text

--- index.php
+++ index.php
@@ -83,7 +83,7 @@
       <div class="intro-text">
         <h1 class="text-center">Dobrodošli na Religija.net</h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          Ovde ćete pronaći različite teme iz Biblije, po nama najvažnije knjige na svetu.  Naš jedini vođa je Bog. Ne pripadamo ni jednoj denominaciji, a svaku institucionalizovanu religiju dozivljavamo uslovno, ljudskom rukotvorinom. Nismo sekta, vec jednostavno Hriscani. Verujemo da se Božijom Crkvom smatraju oni koji prihvate Hrista kao svog Spasitelja i koji oslanjaju svoju veru isključivo na Svetom Pismu.  Misija ovog sajta je da se Istina približi ljudima i da na odgovarajući način slavimo našeg Gospoda i Spasitelja, Isusa Hrista. Nadamo se da će Vas sadržaj zainteresovati, sva Vaša pitanja i predloge možete nam poslati putem KONTAKTA.
         </p>
         <a href="#" class="btn btn-intro">Pročitaj Više</a>
       </div>
