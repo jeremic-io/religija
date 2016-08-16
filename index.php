d78c7afd07656c0385461b1420ab10af5cb0ce87
Dragan Jeremic <jeremic.dragan1991@gmail.com>
2016-08-16 11:28:32 +0200

    Add sidebar

--- index.php
+++ index.php
@@ -97,10 +97,10 @@
     <main class="main-content">
       <div class="container">
         <div class="row">
-          <div class="col-lg-8">
+          <div class="col-lg-9">
 
           </div>
-          <div class="col-lg-4">
+          <div class="col-lg-3">
             <h2>Sidebar</h2>
           </div>
         </div>
