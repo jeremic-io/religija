fde9bd264465bda73a7a65548f865837301f099a
Dragan Jeremic <jeremic.dragan1991@gmail.com>
2016-08-16 09:32:44 +0200

    Add Logo

--- index.php
+++ index.php
@@ -22,7 +22,9 @@
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Religija.net</a>
+            <a class="navbar-brand" href="#">
+              <img src="images/ui/logo.png" class="img img-responsive" alt="Logo">
+            </a>
           </div>
 
           <!-- Collect the nav links, forms, and other content for toggling -->
